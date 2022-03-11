9f3a0933e20916cb6a126cb580e4de8c97962b93
Bruce Perens <bruce@perens.com>
2022-03-10 16:01:26 -0800

    Add console commands for reading, setting, and erasing non-volatile parameters.

---
+++ components/commands/commands.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+// #include "commands.h"
+
+extern void install_param_command(void);
+
+void install_commands(void)
+{
+  install_param_command();
+}
---
+++ components/commands/param.c
@@ -0,0 +1,137 @@
+#include <stdio.h>
+#include <string.h>
+#include <nvs_flash.h>
+#include <nvs.h>
+
+extern nvs_handle_t nvs;
+
+enum parameter_type {
+  END = 0,
+  STRING,
+  INT,
+  FLOAT,
+  URL,
+  DOMAIN
+};
+
+struct parameter {
+  const char * name;
+  enum parameter_type type;
+  bool secret;
+  const char * explanation;
+};
+
+static const struct parameter parameters[] = {
+  { "ssid", STRING, false, "Name of WiFi access point" },
+  { "wifi_password", STRING, true, "Password of WiFi access point" },
+  { }
+};
+
+int list_nvs_params(FILE * f)
+{
+  static const char not_set[] = "not set";
+  static const char secret[] = "(secret)";
+  const struct parameter * p = parameters;
+  char buffer[128];
+  size_t buffer_size;
+
+  while (p->type) {
+    buffer_size = sizeof(buffer);
+    esp_err_t err = nvs_get_str(nvs, p->name, buffer, &buffer_size);
+    if (err != ESP_OK)
+      memcpy(buffer, not_set, sizeof(not_set));
+    if (p->secret)
+      memcpy(buffer, secret, sizeof(secret));
+    printf("%-8s\t%-8s\t%s\n", p->name, buffer, p->explanation);
+    p++;
+  }
+  fflush(f);
+  return 0;
+}
+
+int get_nvs_param(const char * key)
+{
+  char buffer[128];
+  size_t buffer_size;
+
+  const struct parameter * p = parameters;
+  while (p->type) {
+    if (strcmp(p->name, key) == 0)
+      break;
+    p++;
+  }
+  if (!p->type) {
+    fprintf(stderr, "%s is not in the parameter table.\n", key);
+    return -1;
+  }
+
+  buffer_size = sizeof(buffer);
+  esp_err_t err = nvs_get_str(nvs, key, buffer, &buffer_size);
+
+  if (err)
+    printf("not set\n");
+  else {
+    if (p->secret)
+      printf("%s (secret)", key);
+    else
+      printf("%s %s", key, buffer);
+    printf("\n");
+  }
+
+  return 0;
+}
+
+int set_nvs_param(const char * key, const char * value)
+{
+  const struct parameter * p = parameters;
+  while (p->type) {
+    if (strcmp(p->name, key) == 0)
+      break;
+    p++;
+  }
+  if (!p->type) {
+    fprintf(stderr, "%s is not in the parameter table.\n", key);
+    return -1;
+  }
+
+  esp_err_t err = (nvs_set_str(nvs, key, value));
+  if (err) {
+    ESP_ERROR_CHECK(err);
+    return -1;
+  }
+  else {
+    if (p->secret)
+      printf("%s (secret)", key);
+    else
+      printf("%s %s", key, value);
+    printf("\n");
+  }
+  return 0;
+}
+
+int erase_nvs_param(const char * key)
+{
+  const struct parameter * p = parameters;
+  while (p->type) {
+    if (strcmp(p->name, key) == 0)
+      break;
+    p++;
+  }
+  if (!p->type) {
+    fprintf(stderr, "%s is not in the parameter table.\n", key);
+    return -1;
+  }
+
+  esp_err_t err = (nvs_erase_key(nvs, key));
+  switch (err) {
+  case ESP_OK:
+    printf("Erased.\n");
+    return 0;
+  case ESP_ERR_NVS_NOT_FOUND:
+    fprintf(stderr, "%s: was not set.\n", key);
+    return -1;
+  default:
+    ESP_ERROR_CHECK(err);
+    return -1;
+  }
+}

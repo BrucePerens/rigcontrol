0c11012bde8905826c32dadc0e786735e3daddd9
Bruce Perens <bruce@perens.com>
2022-03-25 10:19:57 -0700

    Attempt to make a hamlib component.

---
+++ components/hamlib/include/hamlib/config.h
@@ -0,0 +1,9 @@
+#include <assert.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+#undef __FILENAME__
+#define ABI_VERSION 0
+#define PACKAGE_NAME "hamlib"
+#define PACKAGE_VERSION 0

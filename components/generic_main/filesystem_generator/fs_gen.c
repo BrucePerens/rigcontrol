2dea987ca20e0480f09ea549b457706c9aa37a3d
Bruce Perens <bruce@perens.com>
2022-07-07 10:56:37 -0700

    More comments.

--- components/generic_main/filesystem_generator/fs_gen.c
+++ components/generic_main/filesystem_generator/fs_gen.c
@@ -96,10 +96,10 @@ descend(int fd, const char * name, void (*function)(int fd, const char * name, c
       }
       // d_type values are not the same as (s.st_mode & S_IFMT) values.
       switch ( s.st_mode & S_IFMT ) {
-      case ST_REG:
+      case S_IFREG:
         e->d_type = DT_REG;
         break;
-      case ST_DIR:
+      case S_IFDIR:
         e->d_type = DT_DIR;
         break;
       }
@@ -123,22 +123,44 @@ descend(int fd, const char * name, void (*function)(int fd, const char * name, c
       break;
     default:
       break;
+    }
   }
   closedir(d);
 }
 
+// Write a file to the embedded filesystem. This is used as the coroutine with descend().
+// dir_fd is a file descriptor to the directory containing the file, for openat().
+// name is the name of the file.
+// pathname is relative to the root of the source directory, with no leading slash.
+//
 static void
 write_file(int dir_fd, const char * name, const char * pathname)
 {
+  // stat() is used to get the size of the file.
   struct stat	s;	
+
+  // Pointer to the mapped input file.
   char *	input;
+
+  // File descriptor of the file.
   int		fd = openat(dir_fd, name, O_RDONLY);
+
+  // Used to scan for suffixes of files that should not be compressed.
   const char *	dot = strrchr(name, '.');
+
+  // Number of entries in the suffix table.
   const int	suffix_count = sizeof(do_not_compress_suffixes) / sizeof(*do_not_compress_suffixes);
+
+  // If set, this file should not be compressed.
   bool		do_not_compress = false;
+
+  // Length of the pathname with terminating null.
   int		name_length = 1 + strlen(pathname);
+
+  // Entry for the file in the compressed filesystem.
   struct compressed_fs_entry * e;
 
+  // Grow the number of filesystem entries as necessary.
   if ( entry_index >= entries_size ) {
     entries_size *= 2;
     if ( (entries = realloc(entries, entries_size * sizeof(*entries))) == 0 ) {
@@ -149,14 +171,17 @@ write_file(int dir_fd, const char * name, const char * pathname)
   e = &entries[entry_index];
   entry_index++;
 
+  // Store the file name.
   e->name_offset = output - output_base;
   memcpy(output, pathname, name_length);
   output += name_length;
   output_size -= name_length;
-  e->data_offset = output - output_base;
-
   header->number_of_files++;
 
+  // The file data goes right after the name.
+  e->data_offset = output - output_base;
+
+  // Determine if this file should be compressed.
   if ( dot ) {
     for ( unsigned int i = 0; i < suffix_count; i++ ) {
       if ( strcmp(do_not_compress_suffixes[i], dot) == 0 ) {
@@ -177,21 +202,24 @@ write_file(int dir_fd, const char * name, const char * pathname)
 
   e->size = s.st_size;
 
-
   if ( s.st_size == 0 ) {
     e->compressed_size = 0;
     e->method = ZERO_LENGTH;
     e->data_offset = 0;
   }
   else {
+    // Map the input file.
     if ( (input = mmap(0, s.st_size, PROT_READ, MAP_PRIVATE, fd, 0)) == MAP_FAILED ) {
       fprintf(stderr, "%s: mmap failed: %s\n", pathname, strerror(errno));
       exit(1);
     }
 
+    // libz generally doesn't save space for files smaller than 100 bytes.
+    if ( s.st_size < 100 )
+      do_not_compress = true;
+
     if ( do_not_compress ) {
       // Sendfile would be faster, but it doesn't matter for this application.
-      fprintf(stderr, "copy %d bytes from %lx to %lx\n", s.st_size, input, output);
       memcpy(output, input, s.st_size);
       e->compressed_size = s.st_size;
       e->method = NONE;
@@ -246,22 +274,32 @@ main(int argc, char * * argv)
     fprintf(stderr, "%s: %s\n", argv[2], strerror(errno));
     exit(1);
   }
-  // Can't write an mmapped file with size 0.
-  // Just write the maximum size, and truncate the file to the correct length later.
+  // Can't write a mmapped file with size 0.
+  // ftruncate() sets the file size to our maximum, but doesn't allocate any blocks
+  // to the file. At the end of processing, the file is truncated again, to its actual
+  // length.
   if ( ftruncate(fd, output_base_size) != 0 ) {
     fprintf(stderr, "%s: allocate space failed: %s\n", argv[2], strerror(errno));
     exit(1);
   }
 
+  // Map the output file.
   if ( (output = output_base = mmap(0, output_base_size, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0)) == MAP_FAILED ) {
     fprintf(stderr, "%s: mmap failed: %s\n", argv[2], strerror(errno));
     exit(1);
   }
 
+  // The header is at the start of the output file, and says where everything else is.
+  // The filesystem entries are at the end of the filesystem, after all of the file
+  // names and file data, because their table can
+  // only be written after all files have been processed. File names and file data
+  // are interleaved after the header. Position of things does matter somewhat because
+  // it's a block-based FLASH device.
   header = (struct compressed_fs_header *)output;
   output += sizeof(*header);
   memcpy(header->magic, compressed_fs_magic, sizeof(header->magic));
 
+  // Allocate the initial file table.
   entries_size = 100;
   entries = malloc(entries_size * sizeof(*entries));
   if ( entries == NULL ) {
@@ -269,9 +307,11 @@ main(int argc, char * * argv)
     exit(1);
   }
 
+  // Traverse the source directory, calling write_file() for each file.
   descend(-1, name, write_file);
-  entries_size = sizeof(*entries) * entry_index;
 
+  // Write the file table to the end of the compressed filesystem.
+  entries_size = sizeof(*entries) * entry_index;
   table_offset = output - output_base;
   if ( table_offset & 3 )
     table_offset += (4 - (table_offset & 3));
@@ -280,6 +320,7 @@ main(int argc, char * * argv)
   memcpy(output, entries, entries_size);
   output += entries_size;
 
+  // Write the output file blocks, and set the correct file size.
   file_size = output - output_base;
   msync(output_base, file_size, MS_ASYNC);
   munmap(output_base, output_base_size);
@@ -287,6 +328,7 @@ main(int argc, char * * argv)
     fprintf(stderr, "%s: final truncate failed: %s\n", argv[2], strerror(errno));
     exit(1);
   }
+
   close(fd);
   return 0;
 }
